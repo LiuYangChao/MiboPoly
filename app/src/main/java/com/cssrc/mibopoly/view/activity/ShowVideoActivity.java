a71e1b772dc2e2ac14fdeb973dfe532502861318
LiuYangChao <1302518678@qq.com>
2017-04-11 19:52:06 +0800

    Merge branch 'master' of https://github.com/LiuYangChao/MiboPoly into HEAD
    # Conflicts:
    #	.gitignore

---
+++ app/src/main/java/com/cssrc/mibopoly/view/activity/ShowVideoActivity.java
@@ -0,0 +1,15 @@
+package com.cssrc.mibopoly.view.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.cssrc.mibopoly.R;
+
+public class ShowVideoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_video);
+    }
+}
